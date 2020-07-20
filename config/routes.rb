9667e4b6781b2d8b6188e25e07179b8a863514d3
switch023 <yuuhamada23@gmail.com>
2020-07-20 21:37:26 +0900

    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  root "messages#index";
+  devise_for :users
+  root "groups#index";
+
+  resources :groups, only: [:index, :new, :create, :edit, :update, :destroy] do
+    resources :messages, only: [:index, :create, :destroy]
+  end
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
